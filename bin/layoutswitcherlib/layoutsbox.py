c202424842e3374269dfd1ba759726caa2250e4d
Stefano Capitani <stefano@manjaro.org>
2019-12-19 11:19:49 +0100

    Add handler to block switch button (branding for now) but WIP since seem again loop the function ..

--- bin/layoutswitcherlib/layoutsbox.py
+++ bin/layoutswitcherlib/layoutsbox.py
@@ -18,7 +18,8 @@ css_file = Path('~/.config/gtk-3.0/gtk.css').expanduser()
 temp_dir = tempfile.mkdtemp() 
 
 # Define asset in use
-asset = os.popen("pacman -Qs manjaro-gnome-assets | awk -F '[/ ]' 'NR==1 {print $2}'").read()
+#asset = os.popen("pacman -Qs manjaro-gnome-assets | awk -F '[/ ]' 'NR==1 {print $2}'").read()
+asset = 'manjaro-gnome-assets-19.0'
 
 class Opacity:
     TOP = 1
@@ -48,31 +49,36 @@ def replace_in_file(file_name: str, regex: str, value: str):
             file_write.write(re.sub(pattern, value, line))
     Path(file_old).unlink()
 
-
-def shell(commands):
+def shell(commands) ->tuple:
+    """return true if command return 0 AND error message"""
     if "--dev" in sys.argv:
         print("Debug mode on:")
         print("Simulating shell commands:")
         print(f"{commands}")
     else:
-        # only run if not in dev mode
-        if isinstance(commands, str):
-            # if is string then we only have one shell command
-            subprocess.run(commands, shell=True)
-        else:
-            # is a tuple or list
-            for cmd in commands:
-                subprocess.run(cmd, shell=True)
+        try:
+            # only run if not in dev mode
+            if isinstance(commands, str):
+                # if is string then we only have one shell command
+                subprocess.run(commands, text=True, shell=True, check=True)
+            else:
+                # is a tuple or list
+                for cmd in commands:
+                    subprocess.run(cmd, text=True, shell=True, check=True)
+        except subprocess.CalledProcessError as err:
+            return False, str(err)
+    return True, ""
 
 def rm_brand():
     # commands = tuple
-    subprocess.run(f"pamac-installer --remove manjaro-gdm-branding {asset}", shell=True)
+    commands = (f"pkexec pamac remove --no-confirm manjaro-gdm-branding {asset}"),
+    return shell(commands)
 
 
 
 def rebrand():
-    commands = 'pamac-installer manjaro-gdm-branding',
-    shell(commands)
+    commands = (f"pkexec pamac install --no-confirm {asset}"),
+    return shell(commands)
 
 
 def enable_wayland():
@@ -249,13 +255,12 @@ class LayoutBox(Gtk.Box):
         # Manjaro branding toggle
         manjaro_switch = Gtk.Switch()
         manjaro_switch.set_hexpand(False)
-        #branding_enabled = subprocess.run("pacman -Qq {asset} &>/dev/null", shell=True)
-        branding_enabled = subprocess.run(f"pacman -Qq {asset} &>/dev/null", shell=True)
+        branding_enabled = subprocess.run(f"pacman -Qq {asset} > /dev/null 2>&1", shell=True)
         if branding_enabled.returncode == 0:
             manjaro_switch.set_active(True)
         else:
             manjaro_switch.set_active(False)
-        manjaro_switch.connect("notify::active", self.on_branding_activated)
+        self.eventswitch = manjaro_switch.connect("notify::active", self.on_branding_activated)
         manjaro_label = Gtk.Label()
         manjaro_label.set_markup("        Manjaro branding")
         manjaro_label.props.halign = Gtk.Align.START
@@ -503,11 +508,24 @@ class LayoutBox(Gtk.Box):
 
     def on_branding_activated(self, switch, gparam):
         if switch.get_active():
-            state = "on"
-            rebrand()
+            good, _ = rebrand()
+            if good:
+                state = "on"
+            else:
+                switch.handler_block(self.eventswitch)
+                switch.set_active(False)
+                switch.handler_unblock(self.eventswitch)
+                state = "off"
         else:
-            state = "off"
-            rm_brand()
+            good, _ = rm_brand()
+            if good:
+                state = "off"
+            else:
+                switch.handler_block(self.eventswitch)
+                switch.set_active(True)
+                switch.handler_unblock(self.eventswitch)
+                state = "on"
+
         print("Branding was turned", state)
 
     def on_gnometweaks_activated(self, button):
@@ -523,6 +541,19 @@ class LayoutBox(Gtk.Box):
         """Make images clickable
             change checkbox state and call on_layout_toggled()"""
         box.btn.set_active(True)
+
+    def dialog_error(self, title: str, message:str):
+        """display error modal dialog"""
+        dialog = Gtk.MessageDialog(
+            parent=self.window,
+            flags=Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.CLOSE,
+            message_format=title
+            )
+        dialog.format_secondary_text(message)
+        dialog.run()
+        dialog.destroy()
 
     def on_layoutapply_clicked(self, button):
         """ apply defaut layout to user """
@@ -557,10 +588,17 @@ class LayoutBox(Gtk.Box):
         }
 
         get_extensions(self.layout)
-
+        ret = True
         for cmd in commands.get(self.layout, ""):
-            shell(cmd)
-        with UserConf() as user:
-            self.layout = user.write({'layout':self.layout})
-        print("Layout applied")
-        
+            good, err = shell(cmd)
+            if not good:
+                # here we continue commands ... good idea ??
+                ret = False
+                error = err
+        if not ret:
+            self.dialog_error(f"Error for set layout \"{self.layout}\"", error)
+        else:
+            # save only if not one error
+            with UserConf() as user:
+                self.layout = user.write({'layout':self.layout})
+            print("Layout applied")
