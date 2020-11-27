740f74c2d6b03d43841f6ce82036cffffaa5b00e
Chrysostomus <matti@manjaro.org>
2020-11-27 23:05:08 +0200

    load schemas to right namespace

--- bin/layoutswitcherlib/layoutsbox.py
+++ bin/layoutswitcherlib/layoutsbox.py
@@ -53,7 +53,7 @@ def apply_unity():
         'appindicatorsupport@rgcjonas.gmail.com'
         )
 
-    subprocess.run('dconf load < /usr/share/gls/schemas/unity_layout; \
+    subprocess.run('dconf load /org/gnome/shell/extensions/ < /usr/share/gls/schemas/unity_layout; \
         gsettings --schemadir /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/schemas set org.gnome.shell.extensions.arcmenu menu-layout UbuntuDash; \
         gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"', shell=True)
     
@@ -84,7 +84,7 @@ def apply_classic():
         'window-list@gnome-shell-extensions.gcampax.github.com'
         )
 
-    GLib.spawn_command_line_sync('dconf load < /usr/share/gls/schemas/classc_layout; \
+    GLib.spawn_command_line_sync('dconf load /org/gnome/shell/extensions/ < /usr/share/gls/schemas/classc_layout; \
                 gsettings --schemadir /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/schemas set org.gnome.shell.extensions.arcmenu menu-layout Default; \
                 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"')
     for ext in conflicting_extensions:
