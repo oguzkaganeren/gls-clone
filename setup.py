a315f7f6c09235c0444c90631f403864b59c53f8
Mark Wagie <mark.wagie@tutanota.com>
2021-07-27 08:53:50 -0600

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='layoutswitcherlib',
-    version='0.8.20',
+    version='0.8.22',
     description='Gnome Layout Switcher',
     url='https://gitlab.manjaro.org/Chrysostomus/gnome-layout-switcher',
     download_url='https://gitlab.manjaro.org/Chrysostomus/gnome-layout-switcher',
